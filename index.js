9ddc7c064096b0d05bfbb1d9bb2e39392524f829
Ihar Tsykala <Sudja09@bk.ru>
2019-12-27 01:03:34 +0300

    feat: add rerson into queue

--- index.js
+++ index.js
@@ -1,38 +1,23 @@
-function setBase(a) {
-  let count = 0
-  return function(b) {
-    count = count + b
-    return count + a
+let queue = []
+let id = 1
+
+class Person {
+  constructor(id) {
+    this.id = id
+  }
+  serveTime() {
+    console.log(Math.random() * 10000)
+    return Math.random() * 10000
   }
 }
 
-const inc = setBase(10)
-
-console.log(inc(5))
-console.log(inc(1))
-console.log(inc(8))
-console.log(inc(10))
-
-const obj = {
-  name: "Joe",
-  address: {
-    city: "NYC",
-    street: "street"
-  },
-  schools: ["sc.n1", "sc.n2", { name: "vasya" }],
-  wife: null
+const createPerson = () => {
+  queue = queue.concat(new Person(id))
+  id++
+  if (id > 10) {
+    clearInterval(timerId, 0)
+  }
+  console.log(queue)
 }
 
-function flattenObj(obj) {
-  const arr = Object.values(obj).reduce((acc, item) => {
-    if (typeof item === "string") {
-      return acc.concat(item)
-    }
-    if (item === null) {
-      return acc.concat(null)
-    }
-    return acc.concat(flattenObj(item))
-  }, [])
-  return arr
-}
-console.log(flattenObj(obj))
+let timerId = setInterval(() => createPerson(), Math.random() * 10000)
