06b2a083b7a8ca985c07cc1e4a5ab1128729ebc7
Ihar Tsykala <you@example.comSudja09@bk.ru>
2019-12-27 19:24:10 +0300

    feat: add queue and ATM base logic

--- index.js
+++ index.js
@@ -1,23 +1,120 @@
-let queue = []
-let id = 1
+let queue = [];
+let id = 1;
+let arrATM = [0];
 
+const serveClient = ()=> {
+  const deletePerson = queue.shift()
+}
+class ModelATM {
+  constructor(number) {
+    this.number = number;
+    this.state = false;
+  }
+  changeState() { 
+    this.state=!this.state
+  }
+
+checkFreeBase =()=> {   
+    const deletePerson = queue.shift()
+    item.changeState()
+    console.log(deletePerson)
+}
+
+  checkFree = ()=> {   
+    arrATM.forEach((item)=>{     
+      if(!item.state) {      
+        this.checkFreeBase()
+       setTimeout(()=>item.changeState(),1000)     
+      }    
+    }) 
+    console.log(queue)  
+  }
+
+  checkFree2 = ()=> {
+    if(queue.length) {
+      if(queue.length) {
+        const timerId = setInterval(()=>{
+          arrATM.forEach((item)=>{     
+            if(!item.state) { 
+            checkFreeBase(item)          
+            setTimeout(()=>item.changeState(),2000)
+            if(!queue.length) clearInterval(timerId, 0)   
+            }    
+          })   
+      })     
+       }
+     } 
+  }
+}
+
+const createATM = ()=> {  
+  arrATM = arrATM.map(()=>new ModelATM)    
+}
+
+createATM();
 class Person {
   constructor(id) {
     this.id = id
   }
-  serveTime() {
-    console.log(Math.random() * 10000)
-    return Math.random() * 10000
+  serveTime() {   
+    return Math.random() * 1000
   }
+}
+
+class App {
+  constructor() {
+    let queue = []
+    let id = 1
+    let arrATM = [0];
+  }
+
+  createATM = ()=> {  
+    arrATM = arrATM.map(()=>new ModelATM)    
+  } 
+
+}
+
+checkFreeBase =(item)=> {   
+  const deletePerson = queue.shift()
+  item.changeState()
+  console.log(deletePerson)
+}
+
+const checkFree = (end)=> {   
+  arrATM.forEach((item)=>{     
+    if(!item.state) { 
+      checkFreeBase(item)          
+     setTimeout(()=>item.changeState(),2000)     
+    }    
+  }) 
+  console.log(queue)  
+}
+
+const checkFree2 = ()=> {
+  if(queue.length) {
+    const timerId = setInterval(()=>{
+      arrATM.forEach((item)=>{     
+        if(!item.state) { 
+        checkFreeBase(item)          
+        setTimeout(()=>item.changeState(),2000)
+        if(!queue.length) clearInterval(timerId, 0)   
+        }    
+      })   
+  })     
+   }
 }
 
 const createPerson = () => {
   queue = queue.concat(new Person(id))
   id++
   if (id > 10) {
-    clearInterval(timerId, 0)
-  }
+    clearInterval(timerId, 0)     
+      checkFree2()  
+    }  
   console.log(queue)
+  checkFree();
 }
 
-let timerId = setInterval(() => createPerson(), Math.random() * 10000)
+let timerId = setInterval(() =>  {
+  createPerson()
+}, (Math.random() * 1000))
